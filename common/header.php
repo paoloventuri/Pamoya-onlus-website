a80d4b4aa452816bab84794d3c0430024b95bc52
Paolo Venturi <paolo.venturi.91@gmail.com>
2017-02-19 20:01:47 +0100

    Aggiunti php comuni a tutte le pagine

---
+++ common/header.php
@@ -0,0 +1,10 @@
+<div class="navbar-header">
+    <img id="logo" src="img/logo.jpg" oncontextmenu="return false;">
+    ASSOCIAZIONE PAMOYA - Insieme per crescere
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+</div>
---
+++ common/menu.php
@@ -0,0 +1,37 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li><a href="index.php" title="">Homepage</a></li>
+        <li><a href="statuto_onlus.php" title="">Statuto</a></li>
+        <li><a href="donazioni.php" title="">Per donazioni</a></li>
+        <li><a href="ospedale_ikelu.php" title="">Ospedale di Ikelu</a></li>
+        <li><a href="javascript:;" data-toggle="collapse" data-target="#home1">Step progettuali<i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="home1" class="collapse">         
+                <li><a href="progetto.php" title="">Il Progetto</a></li>
+                <li><a href="inizio_attivita.php" title="">Inizio attività dell'Ospedale</a></li>
+                <li><a href="relazioni.php" title="">Relazioni Medici</a></li>
+                <li><a href="ipotesi_sviluppo.php" title="">Ipotesi di sviluppo</a></li>
+            </ul>
+        </li>
+        <li><a href="vill_tumaini.php" title="">Villaggio Tumaini</a></li>
+        <li><a href="wangingombe_ilembula.php" title="">Wangingombe e Ilembula</a></li>
+        <li><a href="vill_grandi.php" title="">Villaggio Grandi</a></li>
+        <li><a href="s_mamre.php" title="">Scuola Mamre</a></li>
+        <li><a href="javascript:;" data-toggle="collapse" data-target="#home2">Attività Fausta<i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="home2" class="collapse"> 
+                <li><a href="distr_viveri.php" title="">Distribuzione viveri</a></li>
+                <li><a href="gest_scuole.php" title="">Gestione scuole</a></li>
+            </ul>
+        </li>
+        <li><a href="javascript:;" data-toggle="collapse" data-target="#home3">Lettere dall'Africa<i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="home3" class="collapse"> 
+                <li><a href="tarcisio.php" title="">Don Tarcisio</a></li>
+                <li><a href="fausta.php" title="">Maestra Fausta</a></li>
+                <li><a href="archivio_tarcisio.php" title="">Archivio Don Tarcisio</a></li>
+                <li><a href="archivio_fausta.php" title="">Archivio Maestra Fausta</a></li>
+            </ul>
+        <li><a href="immagini_vita_quotidiana.php" title="">Immagini di vita quotidiana</a></li>
+        <li><a href="volontari.php" title="">Spazio Volontari</a></li>
+        <li><a href="stampa.php" title="">Rassegna stampa</a></li>
+        <li><a href="foto_escursioni.php" title="">Escursioni</a></li>
+    </ul>
+</div>
