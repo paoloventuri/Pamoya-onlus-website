70d40c7c3aa887d50e676cf003508dab7ac0ca13
Paolo Venturi <paolo.venturi.91@gmail.com>
2017-02-24 16:57:50 +0100

    Aggiornata pagina di distribuzione viveri

--- distr_viveri.php
+++ distr_viveri.php
@@ -30,6 +30,37 @@
                             Questa attività è stata avviata da Fausta già da parecchi anni, quasi tutte le persone che vengono in questi giorni di distribuzione sono sieropositive oppure orfane, in situazioni di alto disagio, quindi Fusta ha deciso di dare loro le cose essenziali come per esempio; sapone, olio, zucchero, sale, farina proteica fatta da Fausta mescolando vari ingredienti, pesciolini secchi che a loro piacciono tanto. Questo succede una volta al mese, una giornata è dedicata ai poveri del villaggio, un'altra giornata invece per quelli che vengono da villaggi più lontani sempre della Parrocchia di Mtwango, a volte da 10, 11 o 12 Km. di distanza, a piedi naturalmente. In questa attività Fausta si fa aiutare da alcune persone del posto, hanno formato un gruppo che si impegna a svolgere questo compito mensilmente. Purtroppo succedeva che fra le persone bisognose si rubassero le cose distribuite, questo gruppo ha capito che per evitare ciò doveva mettere un certo ordine, suddividendosi così i compiti, ognuno di loro distribuisce un articolo e alla fine i poveri devono passare da Fausta che registra su di una tabella i beni consegnati, occasione anche per parlare con loro, c'è sempre qualcosa da dire e loro hanno sempre qualcosa da chiedere.
                         </p>
                         <br>
+                        <h3>PRASSI PER ACCETTARE RICHIESTE DI AIUTO</h3>
+                        <p>
+                            Ci preme sottolineare che il nostro intervento anche a livello caritativo deve essere educativo, onde non creare nuove forme di sudditanza, di dipendenza, in pratica di nuova schiavitù dagli “wazungu” (europei-bianchi in genere).<br>
+                            Decidere di accogliere una nuova richiesta di aiuto è sempre cosa molto delicata quindi seguiamo una certa prassi. Se una persona viene sola non accogliamo subito la sua richiesta ma ci riserviamo di cercare informazioni nel posto in cui vive rivolgendoci al catechista o ai responsabili caritas o della comunità di base del luogo.<br>
+                            Non accogliamo subito nemmeno le richieste da parte dei responsabili civili di un villaggio poiché potrebbero favorire parenti ed amici imbrogliando con dichiarazioni non veritiere. Accogliamo senza indugio le persone segnalate od accompagnate dal Parroco Don Tarciso perché frequentando i villaggi per le funzioni religiose, conosce meglio di altri la varie famiglie, le varie realtà.<br>
+                            A tal punto, se la persona è accolta nel programma di aiuti raccogliamo tutte le informazioni possibili:<br>
+                            <lu>
+                                <li>se vive sola;</li>
+                                <li>se ha famiglia;</li>
+                                <li>se vedova/vedovo;</li>
+                                <li>se ha figli, quanti, età, scuola, salute, etc;</li>
+                                <li>se sieropositiva o con altre problematiche di salute.</li>
+                            </lu>
+                            Le informazioni vengono registrate su un file personale da poter consultare all'occorrenza ed il nome viene inserito nell'elenco mensile esistente. A questo punto la persona accolta riceverà un kit mensile di cose essenziali per la sopravvivenza.
+                        </p>
+                        <br>
+                        <h3>COSA C'È NEL KIT</h3>
+                        <p>
+                            <lu>
+                                <li>Farina proteica preparata nel mulino della missione macinando frumento, miglio, riso, pescetti secchi e noccioline</li>
+                                <li>Olio per cucinare</li>
+                                <li>Zucchero</li>
+                                <li>Pescetti secchi (per le proteine) da cucinare in umido con la salsina per accompagnare la polenta</li>
+                                <li>Sapone per igiene personale e per il bucato</li>
+                                <li>Sale</li>
+                                <li>Crema per la protezione della pelle</li>
+                                <li>Confezione di thè</li>
+                            </lu>
+                            Ognuno poi riceve vestiti, coperte, scarpe, medicine ed in caso di ricovero ospedaliero anche il denaro poichè qui si paga tutto, anche il cartellino di entrata. Generalmente passiamo noi a pagare direttamente per essere certi che la somma richiesta venga utilizzata per tale scopo.
+                        </p>
+                        <br>
                         <?php slider('img/viveri', 1);?>
                         <br>
                     </div>
