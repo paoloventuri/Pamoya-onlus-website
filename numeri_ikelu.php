6cbf3d94ab7ec2467aca99a636d14b133df03b81
Paolo Venturi <paolo.venturi.91@gmail.com>
2017-07-09 10:58:32 +0200

    Aggiornati i numeri dell'ospedale

--- numeri_ikelu.php
+++ numeri_ikelu.php
@@ -35,9 +35,227 @@
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
-                    <h2>I numeri del St Joseph Hospital di Ikelu - Anno 2016</h2>
+                        <h2>I numeri del St Joseph Hospital di Ikelu - Anno 2016</h2>
 
-
+                        <h3>Visite ambulatoriali</h3>
+                        <table>
+                            <tr>
+                                <td width="25%" align="center"><h4>Mese</h4></td>
+                                <td width="25%" align="center"><h4>Adulti non assicurati</h4></td>
+                                <td width="25%" align="center"><h4>Bambini sotto i 5 anni non assicurati</h4></td>
+                                <td width="25%" align="center"><h4>Adulti assicurati</h4></td>
+                                <td width="25%" align="center"><h4>Bambini sotto i 5 anni assicurati</h4></td>
+                                <td width="25%" align="center"><h4>Totale</h4></td>
+                            </tr>
+                            <tr>
+                                <td align="center">Gennaio</td>
+                                <td align="center">1098</td>
+                                <td align="center">276</td>
+                                <td align="center">501</td>
+                                <td align="center">0</td>
+                                <td align="center">1875</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Febbraio</td>
+                                <td align="center">1829</td>
+                                <td align="center">265</td>
+                                <td align="center">464</td>
+                                <td align="center">0</td>
+                                <td align="center">2558</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Marzo</td>
+                                <td align="center">1286</td>
+                                <td align="center">135</td>
+                                <td align="center">544</td>
+                                <td align="center">0</td>
+                                <td align="center">1965</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Aprile</td>
+                                <td align="center">1415</td>
+                                <td align="center">167</td>
+                                <td align="center">397</td>
+                                <td align="center">0</td>
+                                <td align="center">1979</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Maggio</td>
+                                <td align="center">1738</td>
+                                <td align="center">188</td>
+                                <td align="center">328</td>
+                                <td align="center">0</td>
+                                <td align="center">2254</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Giugno</td>
+                                <td align="center">1463</td>
+                                <td align="center">172</td>
+                                <td align="center">481</td>
+                                <td align="center">0</td>
+                                <td align="center">2116</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Luglio</td>
+                                <td align="center">1346</td>
+                                <td align="center">182</td>
+                                <td align="center">398</td>
+                                <td align="center">0</td>
+                                <td align="center">1926</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Agosto</td>
+                                <td align="center">1342</td>
+                                <td align="center">176</td>
+                                <td align="center">377</td>
+                                <td align="center">39</td>
+                                <td align="center">1934</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Settembre</td>
+                                <td align="center">1321</td>
+                                <td align="center">197</td>
+                                <td align="center">350</td>
+                                <td align="center">56</td>
+                                <td align="center">1924</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Ottobre</td>
+                                <td align="center">1075</td>
+                                <td align="center">178</td>
+                                <td align="center">319</td>
+                                <td align="center">40</td>
+                                <td align="center">1612</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Novembre</td>
+                                <td align="center">1171</td>
+                                <td align="center">164</td>
+                                <td align="center">279</td>
+                                <td align="center">54</td>
+                                <td align="center">1668</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Dicembre</td>
+                                <td align="center">1166</td>
+                                <td align="center">160</td>
+                                <td align="center">290</td>
+                                <td align="center">65</td>
+                                <td align="center">1681</td>
+                            </tr>
+                            <tr>
+                                <td align="center">Totale</td>
+                                <td align="center">16250</td>
+                                <td align="center">2260</td>
+                                <td align="center">4728</td>
+                                <td align="center">254</td>
+                                <td align="center">23492</td>
+                            </tr>
+                        </table>
+                        <br>
+                        <h3>Ricoveri</h3>
+                        <table>
+                            <tr>
+                                <td width="25%" align="center"><h4>Mese</h4></td>
+                                <td width="25%" align="center"><h4>Reparto maschile</h4></td>
+                                <td width="25%" align="center"><h4>Reparto femminile</h4></td>
+                                <td width="25%" align="center"><h4>Pediatria</h4></td>
+                                <td width="25%" align="center"><h4>Maternità</h4></td>
+                                <td width="25%" align="center"><h4>Totale</h4></td>
+                            </tr>
+                            <tr>
+                                <td align="center">Gennaio</td>
+                                <td align="center">117</td>
+                                <td align="center">200</td>
+                                <td align="center">113</td>
+                                <td align="center">127</td>
+                                <td align="center">557</td></tr>
+                            <tr>
+                                <td align="center">Febbraio</td>
+                                <td align="center">121</td>
+                                <td align="center">218</td>
+                                <td align="center">109</td>
+                                <td align="center">104</td>
+                                <td align="center">552</td></tr>
+                            <tr>
+                                <td align="center">Marzo</td>
+                                <td align="center">108</td>
+                                <td align="center">202</td>
+                                <td align="center">101</td>
+                                <td align="center">118</td>
+                                <td align="center">529</td></tr>
+                            <tr>
+                                <td align="center">Aprile</td>
+                                <td align="center">85</td>
+                                <td align="center">140</td>
+                                <td align="center">87</td>
+                                <td align="center">145</td>
+                                <td align="center">457</td></tr>
+                            <tr>
+                                <td align="center">Maggio</td>
+                                <td align="center">111</td>
+                                <td align="center">169</td>
+                                <td align="center">79</td>
+                                <td align="center">185</td>
+                                <td align="center">544</td></tr>
+                            <tr>
+                                <td align="center">Giugno</td>
+                                <td align="center">135</td>
+                                <td align="center">159</td>
+                                <td align="center">82</td>
+                                <td align="center">162</td>
+                                <td align="center">538</td></tr>
+                            <tr>
+                                <td align="center">Luglio</td>
+                                <td align="center">114</td>
+                                <td align="center">209</td>
+                                <td align="center">85</td>
+                                <td align="center">166</td>
+                                <td align="center">574</td></tr>
+                            <tr>
+                                <td align="center">Agosto</td>
+                                <td align="center">103</td>
+                                <td align="center">167</td>
+                                <td align="center">73</td>
+                                <td align="center">169</td>
+                                <td align="center">512</td></tr>
+                            <tr>
+                                <td align="center">Settembre</td>
+                                <td align="center">112</td>
+                                <td align="center">210</td>
+                                <td align="center">89</td>
+                                <td align="center">186</td>
+                                <td align="center">597</td></tr>
+                            <tr>
+                                <td align="center">Ottobre</td>
+                                <td align="center">104</td>
+                                <td align="center">191</td>
+                                <td align="center">86</td>
+                                <td align="center">160</td>
+                                <td align="center">541</td></tr>
+                            <tr>
+                                <td align="center">Novembre</td>
+                                <td align="center">87</td>
+                                <td align="center">143</td>
+                                <td align="center">82</td>
+                                <td align="center">185</td>
+                                <td align="center">497</td></tr>
+                            <tr>
+                                <td align="center">Dicembre</td>
+                                <td align="center">99</td>
+                                <td align="center">128</td>
+                                <td align="center">69</td>
+                                <td align="center">99</td>
+                                <td align="center">395</td></tr>
+                            <tr>
+                                <td align="center">Totale</td>
+                                <td align="center">1296</td>
+                                <td align="center">2136</td>
+                                <td align="center">1055</td>
+                                <td align="center">1806</td>
+                                <td align="center">6293</td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
                 <!-- /.row -->
