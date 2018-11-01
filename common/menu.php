2f37b3cbbb8032c606206dee68ac5d82a6f2347a
Paolo Venturi <paolo.venturi.91@gmail.com>
2018-11-01 11:04:47 +0100

    Aggiunta pagina per progetto stanza prematuri

--- common/menu.php
+++ common/menu.php
@@ -1,35 +1,36 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
-    <ul class="nav navbar-nav side-nav">
-        <li><a href="index.php" title="">Homepage</a></li>
-        <li><a href="statuto_onlus.php" title="">Statuto</a></li>
-        <li><a href="donazioni.php" title="">Per donazioni</a></li>
-        <li><a href="adotta_lettino.php" title="">Adotta un lettino</a></li>
-        <li><a href="javascript:;" data-toggle="collapse" data-target="#home1">Ospedale di Ikelu<i class="fa fa-fw fa-caret-down"></i></a>
-            <ul id="home1" class="collapse">
-                <li><a href="progetto.php" title="">Il Progetto</a></li>
-                <li><a href="foto_ospedale.php" title="">Fotografie dell'ospedale</a></li>
-                <li><a href="relazioni.php" title="">Relazioni Medici</a></li>
-                <li><a href="ipotesi_sviluppo.php" title="">Ipotesi di sviluppo</a></li>
-                <li><a href="bilancio_ikelu.php" title="">Bilancio dell'ospedale</a></li>
-                <li><a href="numeri_ikelu.php" title="">I numeri dell'ospedale</a></li>
-            </ul>
-        </li>
-        <li><a href="vill_tumaini.php" title="">Villaggio Tumaini</a></li>
-        <li><a href="wangingombe_ilembula.php" title="">Wangingombe e Ilembula</a></li>
-        <li><a href="vill_grandi.php" title="">Villaggio Grandi</a></li>
-        <li><a href="s_mamre.php" title="">Scuola Mamre</a></li>
-        <li><a href="distr_viveri.php" title="">Distribuzione viveri</a></li>
-        <li><a href="gest_scuole.php" title="">Scuole materne</a></li>
-        <li><a href="javascript:;" data-toggle="collapse" data-target="#home3">Lettere dall'Africa<i class="fa fa-fw fa-caret-down"></i></a>
-            <ul id="home3" class="collapse">
-                <li><a href="tarcisio.php" title="">Don Tarcisio</a></li>
-                <li><a href="fausta.php" title="">Maestra Fausta</a></li>
-                <li><a href="archivio_tarcisio.php" title="">Archivio Don Tarcisio</a></li>
-                <li><a href="archivio_fausta.php" title="">Archivio Maestra Fausta</a></li>
-            </ul>
-        <li><a href="immagini_vita_quotidiana.php" title="">Immagini di vita quotidiana</a></li>
-        <li><a href="volontari.php" title="">Spazio Volontari</a></li>
-        <li><a href="stampa.php" title="">Rassegna stampa</a></li>
-        <li><a href="foto_escursioni.php" title="">Escursioni</a></li>
-    </ul>
+   <ul class="nav navbar-nav side-nav">
+      <li><a href="index.php" title="">Homepage</a></li>
+      <li><a href="statuto_onlus.php" title="">Statuto</a></li>
+      <li><a href="donazioni.php" title="">Per donazioni</a></li>
+      <li><a href="adotta_lettino.php" title="">Adotta un lettino</a></li>
+      <li><a href="javascript:;" data-toggle="collapse" data-target="#home1">Ospedale di Ikelu<i class="fa fa-fw fa-caret-down"></i></a>
+         <ul id="home1" class="collapse">
+            <li><a href="progetto.php" title="">Il Progetto</a></li>
+            <li><a href="foto_ospedale.php" title="">Fotografie dell'ospedale</a></li>
+            <li><a href="relazioni.php" title="">Relazioni Medici</a></li>
+            <li><a href="ipotesi_sviluppo.php" title="">Ipotesi di sviluppo</a></li>
+            <li><a href="bilancio_ikelu.php" title="">Bilancio dell'ospedale</a></li>
+            <li><a href="numeri_ikelu.php" title="">I numeri dell'ospedale</a></li>
+         </ul>
+      </li>
+      <li><a href="stanza_prematuri.php" title="">Chumba cha watoto njiti</a></li>
+      <li><a href="vill_tumaini.php" title="">Villaggio Tumaini</a></li>
+      <li><a href="wangingombe_ilembula.php" title="">Wangingombe e Ilembula</a></li>
+      <li><a href="vill_grandi.php" title="">Villaggio Grandi</a></li>
+      <li><a href="s_mamre.php" title="">Scuola Mamre</a></li>
+      <li><a href="distr_viveri.php" title="">Distribuzione viveri</a></li>
+      <li><a href="gest_scuole.php" title="">Scuole materne</a></li>
+      <li><a href="javascript:;" data-toggle="collapse" data-target="#home3">Lettere dall'Africa<i class="fa fa-fw fa-caret-down"></i></a>
+         <ul id="home3" class="collapse">
+            <li><a href="tarcisio.php" title="">Don Tarcisio</a></li>
+            <li><a href="fausta.php" title="">Maestra Fausta</a></li>
+            <li><a href="archivio_tarcisio.php" title="">Archivio Don Tarcisio</a></li>
+            <li><a href="archivio_fausta.php" title="">Archivio Maestra Fausta</a></li>
+         </ul>
+      <li><a href="immagini_vita_quotidiana.php" title="">Immagini di vita quotidiana</a></li>
+      <li><a href="volontari.php" title="">Spazio Volontari</a></li>
+      <li><a href="stampa.php" title="">Rassegna stampa</a></li>
+      <li><a href="foto_escursioni.php" title="">Escursioni</a></li>
+   </ul>
 </div>
---
+++ stanza_prematuri.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+   <?php define('DOCROOT', realpath(dirname(__FILE__)).'/'); ?>
+
+   <title>Chumba cha watoto njiti - Associazione Pamoya Onlus</title>
+
+   <?php include DOCROOT.'common/head.php';?>
+</head>
+
+<body>
+   <div id="wrapper">
+      <meta name="viewport" content="width=device-width">
+      <!-- Navigation -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+         <!-- Brand and toggle get grouped for better mobile display -->
+         <?php include DOCROOT.'common/header.php';?>
+
+         <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+         <?php include DOCROOT.'common/menu.php';?>
+         <!-- /.navbar-collapse -->
+      </nav>
+
+      <div id="page-wrapper">
+
+         <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+               <div class="col-lg-12">
+                  <h1 class="page-header">Chumba cha watoto njiti</h1>
+                  <h2 class="page-header">Stanza adibita ai prematuri e/o ai neonati con peso alla nascita inferiore a 2500 g e ai neonati richiedenti assistenza specifica</h2>
+                  <p>
+                     All’interno del reparto di maternità del Saint Joseph Hospital si trova una stanza adibita all’assistenza del neonato pretermine (nato prima della 37a settimana di gestazione) e/o con peso alla nascita inferiore a 2.5 kg, causa del 60-80 % delle morti perinatali secondo i dati della World Health Organization e di un’alta probabilità di morbilità perinatale.
+                     Questa categoria di neonati, infatti, richiede un’assistenza specifica per quanto riguarda la nutrizione, il mantenimento della temperatura corporea, l’igiene generale e del cordone ombelicale e la diagnosi e il trattamento precoce di eventuali infezioni e complicazioni.
+                     Inoltre nella stanza vengono ospitati neonati che hanno bisogno di assistenza specifica e continua come nel caso neonati portatori di patologie e malattie infettive, neonati con un ritardo di crescita o un distress fisico dovuto a varie cause come l’asfissia neonatale, neonati con un indice di Apgar inferiore a 7 dopo la nascita e ai neonati orfani in attesa di un care giver.
+                     <br>
+                     <img class="center" src="img/prematuri/prematuri_01.jpg" oncontextmenu="return false;">
+                     <br>
+                     Una delle tecniche utilizzate per l’assistenza al neonato con un peso alla nascita inferiore a 2.5 kg è la Kangaroo Mother Care (KMC) (vedi Appendice) che prevede il contatto pelle a pelle tra madre e bambino 24 ore su 24 durante il ricovero ospedaliero e/o la permanenza a casa.
+                     Questa tecnica, oltre ad essere ritenta una delle migliori in termini di sicurezza ed efficacia anche qualora l’ospedale offrisse strumenti specifici come l’incubatrice, risulta essere la scelta più economica laddove vi sono insufficienti risorse.
+                     Al momento la stanza offre un piccolo spazio nel quale sono disposti due letti destinati al ricovero della madre accompagnatrice del neonato, un unico lettino con spondine provvisto di un materasso per i neonati che hanno bisogno di assistenza specifica, un mobile con alcuni strumenti per l’assistenza al neonato, una bilancia e una stufa elettrica che riscalda la stanza.
+                     I problemi relativi a suddetta stanza sono:
+                     <ul>
+                        <li>Lo spazio: la stanza ha la capienza di al massimo due letti, insufficienti a ospitare altre mamme per la KMC nel caso ci fossero più di due bambini con un peso alla nascita inferiore a 2500 g. Inoltre il poco spazio costringe a mettere vicini neonati prematuri e neonati portatori di patologie talvolta contagiose.</li>
+                        <li>La dispersione termica: la finestra e la porta sono di legno, e questo è insufficiente al mantenimento di una temperatura adeguata senza un eccessivo consumo della corrente elettrica che alimenta la stufa.</li>
+                        <li>La stufa che viene utilizzata non è sicura e comporta un eccessivo utilizzo di energia elettrica (durante le ore notturne consuma più dell’intero ospedale).</li>
+                        <li>Inadeguato materiale per un’assistenza specifica al neonato.</li>
+                        <li>Mancanza di lavandino e acqua per il lavaggio sociale delle mani.</li>
+                        <li>Mancanza di servizi igienici destinati solo alle mamme che stanno a contatto con i neonati prematuri.</li>
+                     </ul>
+                     Il progetto di ampliare e migliorare la stanza per i prematuri nasce quindi non sono dall’esigenza di migliorare la qualità del servizio ma anche da un aumentato numero di nascite e trasferimenti di neonati con un peso alla nascita inferiore ai 2.5 kg. 
+                     Il Saint Joseph Hospital di Ikelu infatti è l’unico ospedale della zona ad offrire una stanza riscaldata, seppur piccola, per i neonati pretermine e/o con basso peso alla nascita. 
+                     Da Agosto 2017 ad Agosto 2018 sono stati registrati 90 bambini con un peso alla nascita inferiore ai 2.5 kg e 14 morti perinatali su un totale di 1229 nascite.
+                     <br>
+                     Secondo una media calcolata con il numero totale di neonati nati con un peso inferiore a 2.5 kg diviso i mesi di un anno, la stanza accoglierebbe circa 7 madri con 7 neonati a carico in un mese per la durata della Kangaroo Mother Care per un periodo di degenza che può durare settimane e questo dimostra un inadeguato spazio.
+                     Solo il 12% dei 90 neonati con un peso alla nascita inferiore a 2.5 kg da Agosto 2017 ad Agosto 2018 ha usufruito della KMC.
+                     <br>
+                     <img class="center" src="img/prematuri/prematuri_02.jpg" oncontextmenu="return false;">
+
+                     <h3>Progetto</h3>
+                     Il progetto di ampliare la stanza per i neonati con un peso basso alla nascita e adibirne una per i neonati richiedenti assistenza specifica, prevede lo spostamento della stanza per la KMC, adesso adibita ad entrambi i servizi, in una stanza più grande provvista di servizi igienici adesso destinata al ricovero delle mamme dopo parto.
+                     Si farà in modo che nella stanza sia mantenuta una temperatura adeguata per il ricovero delle madri con i loro bambini e per questo motivo saranno cambiate le finestre e la porta e isolate le pareti.
+                     In tale stanza le mamme saranno istruite circa la tecnica della Kangaroo Mother Care.
+                     Inoltre sarà presente e a disposizione delle mamme e del personale sanitario i protocolli e le linee guida della tecnica stessa.
+                     Il personale sanitario sarà formato adeguatamente sulla tecnica della KMC e la cura del neonato con basso peso alla nascita.
+                     Saranno compilati fogli apposta per il controllo dei parametri vitali, l’alimentazione, il peso, gli input e output del neonato.
+                     Saranno presenti le cartelle cliniche di ciascun bimbo. 
+                     La stanza sarà dotata di propri ausili per la pulizia e sarà inoltre possibile cambiarsi le scarpe all’entrata in modo tale da tenere la stanza il più pulita possibile.
+                     La piccola stanza che adesso offre entrambi i servizi sarà invece destinata all’osservazione e al trattamento dei neonati che richiedono una particolare assistenza e che non possono usufruire della KMC (neonati con un peso inferiore a 1200 g, neonati con sepsi o patologie di varia origine).
+                     <br>
+                     <img class="center" src="img/prematuri/prematuri_03.jpg" oncontextmenu="return false;">
+                  
+                     <h3>Lavori</h3>
+                     I lavori quindi includeranno:
+                     <ul>
+                        <li>Isolamento delle pareti;</li>
+                        <li>Cambio di finestre e porte;</li>
+                        <li>Lavori di manutenzione dei servizi igienici.</li>
+                     </ul>
+                     Inoltre verranno aggiunti:
+                     <ul>
+                        <li>5/6 letti con lo schienale regolabile;</li>
+                        <li>2/3 letti/culle per i neonati in attesa;</li>
+                        <li>1/2 mobili per strumenti, medicinali e il materiale di ciascuna madre;</li>
+                        <li>1/2 stufe a basso consumo energetico;</li>
+                        <li>Termometro da parete;</li>
+                        <li>Tiralatte;</li>
+                        <li>Orologio da parete;</li>
+                        <li>Bilancia digitale;</li>
+                        <li>Kangaroo Registration Book;</li>
+                        <li>Ordering Books;</li>
+                        <li>Contenitori per lo smaltimento dei rifiuti;</li>
+                        <li>Ausili per la pulizia;</li>
+                        <li>Sterilizzatore pediatrico.</li>
+                     </ul>
+                     Gli strumenti che dovranno essere sempre presenti nella stanza per l’assistenza specifica saranno:
+                     <ul>
+                        <li>Pulsossimetri per neonati (almeno 3);</li>
+                        <li>Termometri pediatrici;</li>
+                        <li>Ambu pediatrico;</li>
+                        <li>Concentratore di ossigeno;</li>
+                        <li>Aspiratore con cannule;</li>
+                        <li>Tubo naso-gastrico per neonati;</li>
+                        <li>Materiale per terapia (ago cannula pediatrico, garze, siringhe, cotone, disinfettante...);</li>
+                        <li>Medicinale vario;</li>
+                        <li>Coperte o materassino termico;</li>
+                        <li>Isola neonatale;</li>
+                        <li>Incubatrice.</li>
+                     </ul>
+                     <img class="center" src="img/prematuri/prematuri_05.jpg" oncontextmenu="return false;">
+
+                     <h3>Costi</h3>
+                     Qua sotto sono elencati i costi approssimativi del materiale mancante:
+                     <ul>
+                        <li>3 finestre e 2 porte in alluminio: 650 €</li>
+                        <li>Lavori per la stanza e mobili: 1000 €</li>
+                        <li>Termometri da parete: 50 €</li>
+                        <li>Bilancia digitale: 50 €</li>
+                        <li>Stufe per il riscaldamento: 100 €</li>
+                        <li>Coperte/materassini termici: 50 €</li>
+                        <li>Sterilizzatore pediatrico: 70 €</li>
+                        <li>Termometri pediatrici: 20 €</li>
+                        <li>Pulsossimetri per neonati: 300 €</li>
+                        <li>Incubatrice: 5.000 €</li>
+                     </ul>
+                     Per un totale approssimativo di 7.210 euro.
+
+                     <h3>La Kangaroo Mother Care (KMC)</h3>
+                     La tecnica della Kangaroo Mother Care (KMC), destinata ai bambini con un peso alla nascita inferiore ai 2.5 kg e con parametri vitali stabili, prevede contatto pelle a pelle tra madre e bambino 24 ore su 24.
+                     Questo permette una stabilizzazione dei parametri cardio respiratori del bambino e una regolazione del livello della sua temperatura corporea meglio di qualsiasi strumento.  
+                     La tecnica include anche l’allattamento al seno esclusivo e frequente. 
+                     Le evidenze scientifiche sull’utilizzo della Kangaroo Mather Care sui neonati con peso basso ala nascita senza nessun problema medico hanno dimostrato che:
+                     <lu>
+                        <li>La KMC si può paragonare alle cure convenzionali (incubatrici), in termini di sicurezza e protezione termica utilizzando come parametro la mortalità.</li>
+                        <li>La KMC, facilitando l’allattamento materno, offre evidenti vantaggi in caso di severa morbilità. La tecnica contribuisce a l’umanizzazione della cura neonatale e ad un miglior legame tra madre e neonato.</li>
+                        <li>La KMC, sembra abbia un effetto benefico anche sul benessere della madre in termini di stress e autostima. Aumenta l’empowerment in quanto la madre si sente partecipe e collaborante nell’aiutare il proprio bambino facendo qualcosa di positivo per lui.</li>
+                     </lu>   
+                     Secondo alcuni studi, quindi, si conclude che la KMC deve essere incoraggiata, sopratutto nei paesi in cui mancano altri strumenti per la gestione del basso peso alla nascita in quanto migliora l’outcome non solo neonatale ma anche materno e familiare.
+                     <br>
+                     <img class="center" src="img/prematuri/prematuri_04.jpg" oncontextmenu="return false;">
+                  </p>
+               </div>
+            </div>
+            <!-- /.row -->
+
+         </div>
+         <!-- /.container-fluid -->
+
+      </div>
+      <!-- /#page-wrapper -->
+      <?php include DOCROOT.'common/footer.php';?>
+   </div>
+   <!-- /#wrapper -->
+
+   <!-- jQuery -->
+   <script src="js/jquery.js"></script>
+
+   <!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
