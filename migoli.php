b3630a72da6d8a5060ec7f5e129767de9548ce41
Paolo Venturi <paolo.venturi.91@gmail.com>
2023-10-08 20:58:32 +0200

    Progetto Amici di Migoli

---
+++ migoli.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+   <?php define('DOCROOT', realpath(dirname(__FILE__)).'/'); ?>
+
+   <title>Progetto Amici di Migoli - Associazione Pamoya Onlus</title>
+
+   <?php include DOCROOT.'common/head.php';?>
+</head>
+
+<body>
+   <div id="wrapper">
+      <meta name="viewport" content="width=device-width">
+      <!-- Navigation -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+         <!-- Brand and toggle get grouped for better mobile display -->
+         <?php include DOCROOT.'common/header.php';?>
+
+         <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+         <?php include DOCROOT.'common/menu.php';?>
+         <!-- /.navbar-collapse -->
+      </nav>
+
+      <div id="page-wrapper">
+
+         <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+               <div class="col-lg-12">
+                  <h1 class="page-header">Progetto Amici di Migoli</h1>
+                     <p>
+                     <b>Changarawe - Migoli</b> è una circoscrizione mista della Tanzania centrale situata nel distretto rurale di Iringa, in una zona arida e con scarse risorse economiche. La popolazione è a maggioranza Masai e conta circa 18.000 persone. 
+                     <br>
+                     Le <b>Suore Collegine della Sacra Famiglia</b> gestiscono un piccolo Health Center, gli alloggi per i volontari e l'asilo. 
+                     <br>
+                     <br>
+                     <b>Dall'agosto del 2019</b>, grazie al contributo di molti amici e il sostegno delle Associazioni KAMMEA e PAMOYA, sono stati realizzati innanzitutto lavori di <b>manutenzione urgente</b> come rifacimento dei tetti, strutture di sostegno e controsoffittature, ammodernamento degli impianti idrico ed elettrico che hanno consentito la ripartenza delle attività nell'Health Center garantendo alla popolazione l'assistenza sanitaria primaria. 
+                     <br>
+                     <br>
+                     Successivamente è stato possibile eseguire anche <b>lavori straordinari</b> che hanno migliorato le prestazioni offerte e l'efficienza del centro. 
+                     <br>
+                     In particolare, si è provveduto a:
+                     <ul>
+                        <li>messa in opera dei <b>pannelli solari</b> che rendono sia l'Health Center che la casa della Missione indipendente dalla linea elettrica nazionale, soggetta a continui blackout</li>
+                        <li>ammodernamento del <b>reparto di maternità</b></li>
+                        <li>ristrutturazione della <b>sala parto</b> e della sterilizzazione </li>
+                        <li>creazione di un reparto di <b>Emergency Care</b> </li>
+                        <li>riorganizzazione del <b>laboratorio analisi</b> </li>
+                        <li>ristrutturazione della <b>casa di accoglienza</b> per i volontari </li>
+                     </ul>
+                     I <b>progetti attualmente</b> in corso prevedono il <b>rifacimento dell'asilo</b> e l'<b>allestimento</b> di una piccola <b>sala operatoria</b> per poter eseguire tagli cesarei e cure chirurgiche minori.
+                     <br>
+                     Il <b>Progetto “Amici di Migoli”</b> nasce dalla passione di un medico italiano, il dottor Giovanni Cecchini che, insieme alla moglie Daniela, ha dedicato gran parte della sua vita, professionale e non, alla popolazione africana. 
+                     <br>
+                     L'entusiasmo di Giovanni e Daniela ha coinvolto i consuoceri Angelo e Lella Aimetti che hanno raccolto il testimone e hanno coinvolto sempre più amici per poter sostenere la comunità di Changarawe, offrendo risorse umane e materiali per la crescita dell'Health Center e dell'asilo, nell'ottica della <b>promozione del diritto alla salute e all'istruzione</b>, diritti primari per la persona e fondamentali per lo sviluppo dell'intera nazione. 
+                     <br>
+                     Tutte le opere sono realizzate in collaborazione con le Suore Collegine che vivono a Changarawe ed eseguite con manodopera locale. 
+                     <br>
+                     <br>
+                     Dal 2023 il Progetto Amici di Migoli è stato ufficialmente incluso nella mission della Associazione PAMOYA – INSIEME PER CRESCERE ONLUS, di Malonno (BS), che fin da subito ha sostenuto il progetto mettendo a disposizione competenze e mezzi logistici.
+                     <br>
+                     <div class="center">
+                        <img src="img/migoli/migoli_01.jpg" width="560" oncontextmenu="return false;">
+                        <img src="img/migoli/migoli_02.jpg" width="560" oncontextmenu="return false;">
+                     </div>
+                     <h4>
+                        Per accompagnarci nel progetto potete effettuare donazioni sul C.C.:
+                        <br>
+                        ASSOCIAZIONE ONLUS “PAMOYAINSIEMEPERCRESCERE”–
+                        <br>
+                        IBAN: IT87 A083 4054 7100 0080 0200 628 –
+                        con causale: PROGETTO MIGOLI
+                        <br>
+                        <br>
+                        Potrete avere regolare ricevuta della donazione, inviando alla mail indicata copia del bonifico e i vostri dati personali.
+                        <br>
+                        <br>
+                        Grazie!
+                        <br>
+                        Gli Amici di Migoli
+                        <br>
+                        <br>
+                        Referente progetto:
+                        <br>
+                        Angelo Aimetti
+                        <br>
+                        Tel.+393386041782
+                        <br>
+                        E-mail: moses_chiara@alice.it
+                     </h4>
+                     </p>
+                     <div class="center">
+                        <img src="img/migoli/migoli_03.jpg" width="560" oncontextmenu="return false;">
+                        <img src="img/migoli/migoli_04.jpg" width="560" oncontextmenu="return false;">
+                     </div>
+               </div>
+            </div>
+            <!-- /.row -->
+
+         </div>
+         <!-- /.container-fluid -->
+
+      </div>
+      <!-- /#page-wrapper -->
+      <?php include DOCROOT.'common/footer.php';?>
+   </div>
+   <!-- /#wrapper -->
+
+   <!-- jQuery -->
+   <script src="js/jquery.js"></script>
+
+   <!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
